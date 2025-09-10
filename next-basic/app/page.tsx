7ba3f1b5493b59632698094342600e0627468e98
devoil <devdevoildev@gmail.com>
2025-09-10 23:49:26 +0900

    feat: [next-basic] 링크 태그가 좋은 이유
    - 클라이언트 사이드 라우팅에 도움
    - next 에게 URL 프리패칭 힌트 줘서 성능 도움

--- next-basic/app/page.tsx
+++ next-basic/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
+// Link 쓰면 a 태그보다 클라이언트 사이드 라우팅에 도움되고, Next.js에게 URL 프리패칭 힌트를 줘서 성능 향상에 도움됨
 export default function Home() {
-  return <main>HOME</main>;
+  return (
+    <main>
+      HOME
+      <Link href='/dashboard'>Go to Dashboard</Link>
+    </main>
+  );
 }
