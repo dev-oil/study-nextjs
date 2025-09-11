91313700e6bdfe0983baac6bfe0618cdb84bed69
devoil <devdevoildev@gmail.com>
2025-09-11 21:56:30 +0900

    feat: [next-basic] 메타 데이터

--- next-basic/app/page.tsx
+++ next-basic/app/page.tsx
@@ -1,6 +1,14 @@
+import { Metadata } from 'next';
 import Link from 'next/link';
 
 // Link 쓰면 a 태그보다 클라이언트 사이드 라우팅에 도움되고, Next.js에게 URL 프리패칭 힌트를 줘서 성능 향상에 도움됨
+
+// metadata -> SEO 정의하는 방식
+export const metadata: Metadata = {
+  title: 'Next.js Tutorial',
+  description: 'Next.js 튜토리얼 배우기',
+};
+
 export default function Home() {
   return (
     <main>
