daf47b378b309c02fe78c79fbc75aa5ba34fdd14
devoil <devdevoildev@gmail.com>
2025-09-17 20:39:28 +0900

    feat: [next-basic] tanstack query 2 - useMutation

--- next-basic/app/todos/page.tsx
+++ next-basic/app/todos/page.tsx
@@ -3,14 +3,32 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { createTodo, getTodos } from '../actions/todo-actions';
 import { useState } from 'react';
+import { create } from 'domain';
 
 export default function TodosPage() {
+  const [todo, setTodo] = useState('');
+
+  // useQuery는 데이터를 가져온다
   const todosQuery = useQuery({
     queryKey: ['todos'], // 어떤 키 값으로 관리할거냐
     queryFn: () => getTodos(), // 실제로 데이터를 가지고 오는 query 함수
   });
 
-  const [todo, setTodo] = useState('');
+  // useMutation은 데이터를 바꾼다
+  // create delete update
+  const createTodoMutation = useMutation({
+    mutationFn: async () => {
+      if (todo === '') throw new Error('할 일을 입력하세요');
+      return createTodo(todo);
+    },
+    onSuccess: (TODOS) => {
+      todosQuery.refetch(); // mutationFn이 성공했을 때 연관된 todosQuery를 다시 불러오도록
+      setTodo('');
+    },
+    onError: (error) => {
+      alert(error.message);
+    },
+  });
 
   return (
     <div>
@@ -19,10 +37,13 @@ export default function TodosPage() {
       {/* Todos를 생성하는 부분 */}
       <input
         type='text'
+        placeholder='할 일을 입력하세요'
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
-      <button onClick={() => createTodo(todo)}>추가</button>
+      <button onClick={() => createTodoMutation.mutate()}>
+        {createTodoMutation.isPending ? '등록중...' : '등록'}
+      </button>
 
       {/* Todos를 보여주는 부분 */}
       {todosQuery.isLoading && <div>Loading...</div>}
